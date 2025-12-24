739f955775da981e98ac12e8d18191bc65a68ed6
KyungJunMoon <ansrudwns03290@gmail.com>
2025-12-24 16:20:22 +0900

    251224

--- 1463.cpp
+++ 1463.cpp
@@ -1,27 +1,33 @@
 #include<iostream>
+#include<tuple>
 
 using namespace std;
 
-int dp[1000001];
+tuple<int ,int> t[40];
 
 int main(void){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
+    int T;
     int n;
 
-    cin >> n;
+    cin >> T;
 
-    dp[1] = 0;
-    for(int i=2; i<=n; i++){
-        dp[i] = dp[i-1] + 1;
+    get<0>(t[0]) = 1;
+    get<1>(t[0]) = 0;
+    get<0>(t[1]) = 0;
+    get<1>(t[1]) = 1;
+    
+    while(T--){
+        cin >> n;
+        
+        for(int i=2; i<=n; i++){
+            get<0>(t[i]) = get<0>(t[i-1]) +  get<0>(t[i-2]);
+            get<1>(t[i]) = get<1>(t[i-1]) +  get<1>(t[i-2]);
+        }
 
-        if(i%2 == 0)
-            dp[i] = min(dp[i], dp[i/2] + 1);
-        if(i%3 == 0)
-            dp[i] = min(dp[i], dp[i/3] + 1);
+        cout << get<0>(t[n]) << ' ' << get<1>(t[n]) << '\n';
     }
-
-    cout << dp[n] << '\n';
     return 0;
 }
