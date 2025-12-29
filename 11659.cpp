2ecb9a1cca9bb8f37447f2c460795695e7c2a069
KyungJunMoon <ansrudwns03290@gmail.com>
2025-12-29 13:39:18 +0900

    timeout

--- 11659.cpp
+++ 11659.cpp
@@ -2,39 +2,27 @@
 
 using namespace std;
 
-int num[100001];
 int sum[100001];
 
-int main(void){
+int main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
     int N, M;
-    
+    int a, b;
+
     cin >> N >> M;
 
     for(int i=1; i<=N; i++){
         int temp;
-        
         cin >> temp;
-        num[i] = temp;
-        if(i!=1){
-            sum[i-1] = num[i-1] + temp;
-        }
+        sum[i] = sum[i-1] + temp;
     }
 
     for(int i=0; i<M; i++){
-        int a, b;
-        int res=0;
-        int j;
-
         cin >> a >> b;
-        for(j=a; j<b; j+=2){
-            res += sum[j];
-        }
-
-        if(j==b){
-            res += num[j];
-        }
-
-        cout << res << '\n';
+        cout << sum[b] - sum[a-1] << '\n';
     }
 
     return 0;
